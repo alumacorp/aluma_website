db7439331afb96b2237095431a85e4b8ab914059
aryan0078 <aryan8931080555@gmail.com>
2022-03-07 02:02:21 +0530

    [DESIGN CHANGES]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./components.css";
+import Drawer from "./Drawer";
 export default function Header() {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
   return (
     <div className="header">
+      {drawerOpen && <Drawer close={setDrawerOpen} />}
       <img className="logo" src={require("../assets/logo.png")} />
-      <div className="header-right">
-        <p>Home</p>
-        <p>About Us</p>
-        <p>Our Services</p>
-        <p>Team</p>
-        <button className="header-button">
-          Download App{" "}
-          <i class="fab fa-google-play" style={{ color: "rgb(161 95 205)" }} />
-        </button>
+      <div className="header-mobile">
+        <i class="fas fa-bars" onClick={() => setDrawerOpen(true)} />
+      </div>
+      <div className="header-desktop">
+        <div className="header-right">
+          <p>Home</p>
+          <p>About Us</p>
+          <p>Our Services</p>
+          <p>Team</p>
+          <button className="header-button">
+            Download App{" "}
+            <i
+              class="fab fa-google-play"
+              style={{ color: "rgb(161 95 205)" }}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,26 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "./Home.css";
 export default function Home() {
+
   return (
-    <div className="home">
+    <>
       <Header />
-      <h1>
-        Making life
-        <span className="tag-line" style={{ color: "rgb(161 95 205)" }}>
-          easy
-        </span>
-      </h1>
-      <img className="phone-image" src={require("../assets/phone.png")} />
-      <button className="download-button">
-        Coming soon <i class="fab fa-apple" />
-      </button>
-      <Footer />
-    </div>
+      <div className="home">
+        <h1>
+          Making life
+          <a
+            className="tag-line"
+            style={{ color: "rgb(161 95 205)", marginLeft: "15px" }}
+          >
+            easy
+          </a>
+        </h1>
+        <img className="phone-image" src={require("../assets/phone.png")} />
+        <button className="download-button">
+          Coming soon <i class="fab fa-apple" />
+        </button>
+        <Footer />
+      </div>
+    </>
   );
 }
