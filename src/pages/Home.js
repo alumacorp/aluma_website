bb3f55b6c43b8127d141cd1b3f4a7880f5eaf86f
aryan0078 <aryan8931080555@gmail.com>
2022-04-20 15:17:05 +0530

    [patch]

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,298 @@
+import { Popover, Transition } from "@headlessui/react";
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+  SpeakerphoneIcon,
+  XIcon,
+} from "@heroicons/react/outline";
 import React from "react";
+import Typewriter from "typewriter-effect";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "./Home.css";
+const features = [
+  {
+    name: "Competitive exchange rates",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: GlobeAltIcon,
+  },
+  {
+    name: "No hidden fees",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: ScaleIcon,
+  },
+  {
+    name: "Transfers are instant",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: LightningBoltIcon,
+  },
+  {
+    name: "Mobile notifications",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: AnnotationIcon,
+  },
+];
 export default function Home() {
-
   return (
     <>
       <Header />
-      <div className="home">
-        <h1>
-          Making life
-          <a
-            className="tag-line"
-            style={{ color: "rgb(161 95 205)", marginLeft: "15px" }}
-          >
-            easy
-          </a>
-        </h1>
-        <img className="phone-image" src={require("../assets/phone.png")} />
-        <button className="download-button">
-          Coming soon <i class="fab fa-cut"/>
-        </button>
-        <Footer />
+      <div className="relative bg-black overflow-hidden">
+        <div className="bg-orange-400">
+          <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between flex-wrap">
+              <div className="w-0 flex-1 flex items-center">
+                <span className="flex p-2 rounded-lg bg-orange-800">
+                  <SpeakerphoneIcon
+                    className="h-6 w-6 text-white"
+                    aria-hidden="true"
+                  />
+                </span>
+                <p className="ml-3 font-medium text-white truncate">
+                  <span className="md:hidden">We announced a new product!</span>
+                  <span className="hidden md:inline">
+                    We are rolling out beta soon!
+                  </span>
+                </p>
+              </div>
+              <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-orange-400 bg-white hover:bg-orange-50"
+                >
+                  Learn more
+                </a>
+              </div>
+              <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+                <button
+                  type="button"
+                  className="-mr-1 flex p-2 rounded-md hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+                >
+                  <span className="sr-only">Dismiss</span>
+                  <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-7xl mx-auto">
+          <div className="relative  pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <svg
+              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-black transform translate-x-1/2"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <main className="mt-10 mx-auto max-w-8xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">Making Life Easy</span>{" "}
+                  <span className="block text-orange-400 xl:inline">
+                    <Typewriter
+                      options={{
+                        strings: [
+                          "Book services",
+                          "Pay offlie or online",
+                          "Get nearby services",
+                        ],
+                        autoStart: true,
+                        loop: true,
+                      }}
+                    />
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                  occaecat fugiat aliqua.
+                </p>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="rounded-md shadow">
+                    <a
+                      href="#"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-400 hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
+                    >
+                      Get started
+                    </a>
+                  </div>
+                  <div className="mt-3 sm:mt-0 sm:ml-3">
+                    <a
+                      href="#"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-orange-700 bg-orange-100 hover:bg-orange-200 md:py-4 md:text-lg md:px-10"
+                    >
+                      Live demo
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+
+        <div className="py-12 bg-black">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="lg:text-center">
+              <h2 className="text-base text-orange-400 font-semibold tracking-wide uppercase">
+                Features
+              </h2>
+              <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-100 sm:text-4xl">
+                A better way to make your life easy
+              </p>
+              <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+                magnam voluptatum cupiditate veritatis in accusamus quisquam.
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative">
+                    <dt>
+                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-orange-500 text-white">
+                        <feature.icon className="h-6 w-6" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-300">
+                        {feature.name}
+                      </p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500">
+                      {feature.description}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div className="relative bg-black overflow-hidden">
+          <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+            <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
+              <div className="sm:max-w-lg">
+                <h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+                  Beta look of the service
+                </h1>
+                <p className="mt-4 text-xl text-gray-500">
+                  This year, our new summer collection will shelter you from the
+                  harsh elements of a world that doesn't care if you live or
+                  die.
+                </p>
+              </div>
+              <div>
+                <div className="mt-10">
+                  {/* Decorative image grid */}
+                  <div
+                    aria-hidden="true"
+                    className="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full"
+                  >
+                    <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                      <div className="flex items-center space-x-6 lg:space-x-8">
+                        <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                          <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                        </div>
+                        <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                        </div>
+                        <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                          <div className="w-44 h-64 rounded-lg overflow-hidden">
+                            <img
+                              src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <a
+                    href="#"
+                    className="inline-block text-center bg-orange-400 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-orange-700"
+                  >
+                    Want beta ?
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="bg-black dark:bg-gray-800 ">
+          <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
+            <h2 class="text-3xl font-extrabold text-gray-600 dark:text-white sm:text-4xl">
+              <span class="block">Want to be part of Us ?</span>
+              <span class="block text-orange-500">
+                It&#x27;s today or never.
+              </span>
+            </h2>
+            <p class="text-xl mt-4 max-w-md mx-auto text-gray-400">
+              I had noticed that both in the very poor and very rich extremes of
+              society the mad were often allowed to mingle freely
+            </p>
+            <div class="lg:mt-0 lg:flex-shrink-0">
+              <div class="mt-12 inline-flex rounded-md shadow">
+                <button
+                  type="button"
+                  class="py-4 px-6  bg-orange-400 hover:bg-orange-700 focus:ring-orange-500 focus:ring-offset-orange-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                >
+                  Get started
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
