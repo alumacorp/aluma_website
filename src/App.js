e0de06cf011e87737f2e340d3148de3934797111
Aryan Singh <aryan8931080555@gmail.com>
2022-05-22 05:05:54 +0530

    [SUB DOMAIN PATCH]

--- src/App.js
+++ src/App.js
@@ -7,19 +7,43 @@ import OurTeam from "./pages/OurTeam";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ContactUs from "./components/ContactUs";
+import { useEffect, useState } from "react";
+import MemberVerification from "./pages/dashboard/MemberVerification";
 function App() {
+  const [subDomain, setSubDomain] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  useEffect(() => {
+    const host = window.location.host;
+    const arr = host.split(".").slice(0, 1);
+    if (arr.length > 0) {
+      if (arr[0] == "admin") setIsAdmin(true);
+      setSubDomain(arr[0]);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/our-team" element={<OurTeam />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-      </Routes>
-      <Footer />
+      {isAdmin ? (
+        <>
+          <Routes>
+            <Route path="/" element={<MemberVerification />} />
+          </Routes>
+        </>
+      ) : (
+        <>
+          {" "}
+          <Header />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/our-team" element={<OurTeam />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 }
---
+++ src/pages/dashboard/MemberVerification.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MemberVerification() {
+  return <div>MemberVerification</div>;
+}
